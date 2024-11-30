4529a1f4456cfccadcbc21c33512737dbee71ab6
dawid <dawtom97@gmail.com>
2024-11-30 10:03:30 +0100

    initial commit

---
+++ constants/settings.py
@@ -0,0 +1,4 @@
+CITY = "Paris"
+API_KEY = "863ad5508956bc04f81834a8161ceebd"
+API_URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
+OUTPUT_FILE = "pogoda.xlsx"
---
+++ utils/fetch_weather_records.py
@@ -0,0 +1,20 @@
+from utils.mysql_connect import mysql_connect
+import pandas as pd
+
+def fetch_weather_records():
+    connection = mysql_connect()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        query = "SELECT * FROM weather_records"
+
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        columns = ["a","b","c"]
+        # df = pd.DataFrame(rows, columns)
+
+        print(rows)
+
+        connection.close()
+
+
+fetch_weather_records()
